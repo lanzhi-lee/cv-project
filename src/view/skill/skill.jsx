237693d12c7f06d91daa042627f079179f2a6ff9
Lee <baibai_lee@163.com>
2019-09-02 00:54:23 +0800

    feat 新增专业技能

--- src/view/skill/skill.jsx
+++ src/view/skill/skill.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './index.scss'
 
 const Skill = () => {
     return (
-        <section className="skill">Skill</section>
+        <section className="skill">
+            {/* <h1>专业技能</h1> */}
+            <div className="skill-list">
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>JavaScript</span>
+                <span>ECharts</span>
+                <span>Node.js</span>
+                <span>Vue</span>
+                <span>React</span>
+                <span>Mock.js</span>
+                <span>Koa</span>
+            </div>
+        </section>
     )
 }
 
