af417649e1bdcf8fa659a84eb292514d34615372
lizhan <baibai_lee@163.com>
2019-09-03 03:44:05 +0800

    0903晚暂存

--- src/view/edu/edu.jsx
+++ src/view/edu/edu.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './index.scss'
 
 const Edu = () => {
+    function setExpTouch3D() {
+        const expDom = document.getElementsByClassName('edu__main')[0];
+        const contentDom = expDom.parentNode;
+        expDom.addEventListener('mousemove', throttleGenerator(expMousemoveHandler, 20));
+        expDom.addEventListener('mouseout', expMouseoutHandler);
+
+        function throttleGenerator(fn, time) {
+            let date = new Date();
+            return (...args) => {
+                const nowDate = new Date();
+                if (nowDate - date > time) {
+                    fn(...args);
+                    date = nowDate;
+                }
+            };
+        }
+
+        function expMousemoveHandler(e) {
+            const pageX = e.pageX;
+            const pageY = e.pageY;
+            const bannerWidth = expDom.offsetWidth;
+            const bannerHeight = expDom.offsetHeight;
+            const offsetLeft = expDom.offsetLeft;
+            const offsetTop = contentDom.offsetTop;
+            const x = pageX - offsetLeft - bannerWidth / 2;
+            const y = -(pageY - offsetTop - bannerHeight / 2);
+            expDom.style.transform = `rotateY(${x / 50}deg) rotateX(${y / 25}deg)`;
+        }
+
+        function expMouseoutHandler() {
+            expDom.style.transform = 'rotateY(0deg) rotateX(0deg)';
+        }
+    }
+
+    useEffect(() => {
+        setExpTouch3D();
+    }, [])
+
     return (
-        <section className="edu">Edu</section>
+        <section className="edu">
+            <div className="edu__main">
+                <div className="edu__main-1">
+                    <h2>教育经历</h2>
+                    <p>于2019.9-2020.6期间就读于河南大学软件学院网络工程专业</p>
+                </div>
+                <div className="edu__main-2">
+                    <h2>校内活动</h2>
+                    <p>河南大学新媒体运营中心</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/view/skill/skill.jsx
+++ src/view/skill/skill.jsx
@@ -1,22 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 
+const SkillListItem = props => {
+    const { selfClassName, iconName, content, infolist } = props
+
+    const [basicStyle, setBasicStyle] = useState({})
+    const [infoStyle, setInfoStyle] = useState({})
+
+    const handleMouseEnter = () => {
+        setBasicStyle({ opacity: 0 })
+        setInfoStyle({ opacity: 1 })
+    }
+
+    const handleMouseLeave = () => {
+        setBasicStyle({ opacity: 1 })
+        setInfoStyle({ opacity: 0 })
+    }
+
+    return (
+        <span className={`skill__list-items skill__list-${selfClassName}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+
+            <span className="skill__list-items-basic" style={basicStyle}>
+                <svg className="icon" aria-hidden="true">
+                    <use xlinkHref={`#icon-${iconName}`}></use>
+                </svg>
+                <span>{content}</span>
+            </span>
+
+            <span className="skill__list-items-info" style={infoStyle}>
+                <ul>
+                    {
+                        infolist.map((info, index) => (
+                            <li key={index}>{info}</li>
+                        ))
+                    }
+                </ul>
+            </span>
+        </span>
+    )
+}
+
 const Skill = () => {
+
+    const skillListData = [
+        { selfClassName: 'basic', iconName: 'HTML', content: 'HTML', infolist: [10, 2, 3] },
+        { selfClassName: 'basic', iconName: 'css', content: 'CSS', infolist: [] },
+        { selfClassName: 'basic', iconName: 'javascript-original', content: 'JavaScript', infolist: [] },
+        { selfClassName: 'framework', iconName: 'React', content: 'React', infolist: [] },
+        { selfClassName: 'framework', iconName: 'JS', content: 'Mock.js', infolist: [] },
+        { selfClassName: 'backend', iconName: 'Nodejs', content: 'Node.js', infolist: [] },
+        { selfClassName: 'tools', iconName: 'git', content: 'Git', infolist: [] },
+        { selfClassName: 'tools', iconName: 'webpack', content: 'Webpack', infolist: [] },
+    ]
+
     return (
         <section className="skill">
-            {/* <h1>专业技能</h1> */}
-            <div className="skill-list">
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>JavaScript</span>
-                <span>ECharts</span>
-                <span>Node.js</span>
-                <span>Vue</span>
-                <span>React</span>
-                <span>Mock.js</span>
-                <span>Koa</span>
+            <div className="skill__list">
+                {
+                    skillListData.map((listitem, index) => {
+                        const { selfClassName, iconName, content, infolist } = listitem
+                        return <SkillListItem key={index} {...{ selfClassName, iconName, content, infolist }} />
+                    })
+                }
+
+                {/* 
+                    基础 html css js 三大件 sass typescript
+                    工具库 jquery echarts
+                    开发工具 git webpack mock.js
+                    前端框架 react vue 
+                */}
             </div>
-        </section>
+        </section >
     )
 }
 
